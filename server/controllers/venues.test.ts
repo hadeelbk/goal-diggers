ddfff7a59e736981fd5fbc17f14b2a5406215f92
JonasCodeworks <codeworks@artyfakt.org>
2024-11-19 10:07:41 +0100

    feat: tests for venues controller done

---
+++ server/controllers/venues.test.ts
@@ -0,0 +1,33 @@
+import express from "express";
+const router = require("../router");
+import supertest from "supertest";
+import * as mongoose from "mongoose";
+const databaseName = "gd-test";
+
+describe("User Component", () => {
+
+  const app = express();
+  app.use(express.json());
+  app.use(router);
+  const request = supertest(app);
+
+  // Setup connection to test db
+  beforeAll(async () => {
+    const url = "mongodb://127.0.0.1/" + databaseName;
+    await mongoose.connect(url);
+  });
+
+  // Closes db connection
+  afterAll(async () => {
+    await mongoose.disconnect();
+  });
+
+  it("comes with predefined venues and serves them from /venues", async () => {
+    const { default: Venue } = await import("../models/venue") // import module dynamically in order to await prefilling
+    const dbVenues = await Venue.find({});
+    const { body: apiVenues } = await request.get("/venues");
+    expect(dbVenues.length > 0).toBeTruthy();
+    expect(dbVenues.length).toBe(apiVenues.length);
+  })
+
+})
